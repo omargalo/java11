9d74c52f9f7ed8d469588692b3b4286cfd1bd2b0
Omar García López <omar.garcia@omargl.net>
2023-12-19 09:58:32 -0600

    Clothing Constructor

--- ShopApp/src/shopapp/ShopApp.java
+++ ShopApp/src/shopapp/ShopApp.java
@@ -25,33 +25,33 @@ public class ShopApp {
         //c1.setName("Pinky");
         //c1.setSize("S");
 
-        Clothing item1 = new Clothing();
-        Clothing item2 = new Clothing();
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
-        Clothing[] items = {item1,item2, new Clothing(), new Clothing()};
+        Clothing[] items = {item1,item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue T-Shirt", 10.5, "S")};
 
-        item1.setDescription("Blue Jacket");
+        /* item1.setDescription("Blue Jacket");
         item1.setPrice(20.9);
         item1.setSize("M");
 
         item2.setDescription("Orange T-Shirt");
         item2.setPrice(10.5);
-        item2.setSize("S");
+        item2.setSize("S"); */
 
-        items[2].setDescription("Green Scarf");
+        /* items[2].setDescription("Green Scarf");
         items[2].setPrice(5.0);
         items[2].setSize("S");
 
         items[3].setDescription("Blue T-Shirt");
         items[3].setPrice(10.5);
-        items[3].setSize("S");
+        items[3].setSize("S"); */
 
         //System.out.println("Item 1" + "," + item1.description + "," + item1.price + "," + item1.size);
         //System.out.println("Item 2" + "," + item2.description + "," + item2.price + "," + item2.size);
 
         //total = (item1.price + item2.price *2) * (1 + tax);
 
-        int measurement = 8;
+        //int measurement = 8;
 
         c1.addItems(items);
 
